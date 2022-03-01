71e6d552d402d226800dda5654b37ecdef3da185
x8lxl8x <58623820+x8lxl8x@users.noreply.github.com>
2022-03-01 07:07:05 +0200

    01.03.2022 07:07

--- home/apps/tc/news-reader/003.php
+++ home/apps/tc/news-reader/003.php
@@ -4,8 +4,8 @@ $config_array =
   "config_enabled"  => "true",
   "config_type"     => "link",
 
-  "media_name"      => "БTA - Page 1",
-  "media_url"       => "http://www.bta.bg/bg/page/214/%D0%92%D0%BE%D0%B4%D0%B5%D1%89%D0%B8%D1%82%D0%B5%20%D0%BD%D0%BE%D0%B2%D0%B8%D0%BD%D0%B8",
+  "media_name"      => "БTA - България",
+  "media_url"       => "https://bta.bg/bg/news/bulgaria",
   "media_encode"    => "false",
 
   "module_body"     => "div[class=top-stories]",
