b802c0a4cc7aeb969740d7badc19e7280f011423
x8lxl8x <58623820+x8lxl8x@users.noreply.github.com>
2022-04-25 07:07:13 +0300

    25.04.2022 07:07

---
+++ home/apps/ln/ua-conversations/archive/texts/00/get-html.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+# -v verbose
+# -s silent
+# -H header
+# -A user agent
+
+user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
+
+prefix='https://www.50languages.com/phrasebook/lesson/uk/ru/'
+
+sleep 2; echo '001'; curl -s -A "${user_agent}" "${prefix}/1/"   -o 001.html
+sleep 2; echo '002'; curl -s -A "${user_agent}" "${prefix}/2/"   -o 002.html
+sleep 2; echo '003'; curl -s -A "${user_agent}" "${prefix}/3/"   -o 003.html
+sleep 2; echo '004'; curl -s -A "${user_agent}" "${prefix}/4/"   -o 004.html
+sleep 2; echo '005'; curl -s -A "${user_agent}" "${prefix}/5/"   -o 005.html
+sleep 2; echo '006'; curl -s -A "${user_agent}" "${prefix}/6/"   -o 006.html
+sleep 2; echo '007'; curl -s -A "${user_agent}" "${prefix}/7/"   -o 007.html
+sleep 2; echo '008'; curl -s -A "${user_agent}" "${prefix}/8/"   -o 008.html
+sleep 2; echo '009'; curl -s -A "${user_agent}" "${prefix}/9/"   -o 009.html
+sleep 2; echo '010'; curl -s -A "${user_agent}" "${prefix}/10/"  -o 010.html
+sleep 2; echo '011'; curl -s -A "${user_agent}" "${prefix}/11/"  -o 011.html
+sleep 2; echo '012'; curl -s -A "${user_agent}" "${prefix}/12/"  -o 012.html
+sleep 2; echo '013'; curl -s -A "${user_agent}" "${prefix}/13/"  -o 013.html
+sleep 2; echo '014'; curl -s -A "${user_agent}" "${prefix}/14/"  -o 014.html
+sleep 2; echo '015'; curl -s -A "${user_agent}" "${prefix}/15/"  -o 015.html
+sleep 2; echo '016'; curl -s -A "${user_agent}" "${prefix}/16/"  -o 016.html
+sleep 2; echo '017'; curl -s -A "${user_agent}" "${prefix}/17/"  -o 017.html
+sleep 2; echo '018'; curl -s -A "${user_agent}" "${prefix}/18/"  -o 018.html
+sleep 2; echo '019'; curl -s -A "${user_agent}" "${prefix}/19/"  -o 019.html
+sleep 2; echo '020'; curl -s -A "${user_agent}" "${prefix}/20/"  -o 020.html
+sleep 2; echo '021'; curl -s -A "${user_agent}" "${prefix}/21/"  -o 021.html
+sleep 2; echo '022'; curl -s -A "${user_agent}" "${prefix}/22/"  -o 022.html
+sleep 2; echo '023'; curl -s -A "${user_agent}" "${prefix}/23/"  -o 023.html
+sleep 2; echo '024'; curl -s -A "${user_agent}" "${prefix}/24/"  -o 024.html
+sleep 2; echo '025'; curl -s -A "${user_agent}" "${prefix}/25/"  -o 025.html
+sleep 2; echo '026'; curl -s -A "${user_agent}" "${prefix}/26/"  -o 026.html
+sleep 2; echo '027'; curl -s -A "${user_agent}" "${prefix}/27/"  -o 027.html
+sleep 2; echo '028'; curl -s -A "${user_agent}" "${prefix}/28/"  -o 028.html
+sleep 2; echo '029'; curl -s -A "${user_agent}" "${prefix}/29/"  -o 029.html
+sleep 2; echo '030'; curl -s -A "${user_agent}" "${prefix}/30/"  -o 030.html
+sleep 2; echo '031'; curl -s -A "${user_agent}" "${prefix}/31/"  -o 031.html
+sleep 2; echo '032'; curl -s -A "${user_agent}" "${prefix}/32/"  -o 032.html
+sleep 2; echo '033'; curl -s -A "${user_agent}" "${prefix}/33/"  -o 033.html
+sleep 2; echo '034'; curl -s -A "${user_agent}" "${prefix}/34/"  -o 034.html
+sleep 2; echo '035'; curl -s -A "${user_agent}" "${prefix}/35/"  -o 035.html
+sleep 2; echo '036'; curl -s -A "${user_agent}" "${prefix}/36/"  -o 036.html
+sleep 2; echo '037'; curl -s -A "${user_agent}" "${prefix}/37/"  -o 037.html
+sleep 2; echo '038'; curl -s -A "${user_agent}" "${prefix}/38/"  -o 038.html
+sleep 2; echo '039'; curl -s -A "${user_agent}" "${prefix}/39/"  -o 039.html
+sleep 2; echo '040'; curl -s -A "${user_agent}" "${prefix}/40/"  -o 040.html
+sleep 2; echo '041'; curl -s -A "${user_agent}" "${prefix}/41/"  -o 041.html
+sleep 2; echo '042'; curl -s -A "${user_agent}" "${prefix}/42/"  -o 042.html
+sleep 2; echo '043'; curl -s -A "${user_agent}" "${prefix}/43/"  -o 043.html
+sleep 2; echo '044'; curl -s -A "${user_agent}" "${prefix}/44/"  -o 044.html
+sleep 2; echo '045'; curl -s -A "${user_agent}" "${prefix}/45/"  -o 045.html
+sleep 2; echo '046'; curl -s -A "${user_agent}" "${prefix}/46/"  -o 046.html
+sleep 2; echo '047'; curl -s -A "${user_agent}" "${prefix}/47/"  -o 047.html
+sleep 2; echo '048'; curl -s -A "${user_agent}" "${prefix}/48/"  -o 048.html
+sleep 2; echo '049'; curl -s -A "${user_agent}" "${prefix}/49/"  -o 049.html
+sleep 2; echo '050'; curl -s -A "${user_agent}" "${prefix}/50/"  -o 050.html
+sleep 2; echo '051'; curl -s -A "${user_agent}" "${prefix}/51/"  -o 051.html
+sleep 2; echo '052'; curl -s -A "${user_agent}" "${prefix}/52/"  -o 052.html
+sleep 2; echo '053'; curl -s -A "${user_agent}" "${prefix}/53/"  -o 053.html
+sleep 2; echo '054'; curl -s -A "${user_agent}" "${prefix}/54/"  -o 054.html
+sleep 2; echo '055'; curl -s -A "${user_agent}" "${prefix}/55/"  -o 055.html
+sleep 2; echo '056'; curl -s -A "${user_agent}" "${prefix}/56/"  -o 056.html
+sleep 2; echo '057'; curl -s -A "${user_agent}" "${prefix}/57/"  -o 057.html
+sleep 2; echo '058'; curl -s -A "${user_agent}" "${prefix}/58/"  -o 058.html
+sleep 2; echo '059'; curl -s -A "${user_agent}" "${prefix}/59/"  -o 059.html
+sleep 2; echo '060'; curl -s -A "${user_agent}" "${prefix}/60/"  -o 060.html
+sleep 2; echo '061'; curl -s -A "${user_agent}" "${prefix}/61/"  -o 061.html
+sleep 2; echo '062'; curl -s -A "${user_agent}" "${prefix}/62/"  -o 062.html
+sleep 2; echo '063'; curl -s -A "${user_agent}" "${prefix}/63/"  -o 063.html
+sleep 2; echo '064'; curl -s -A "${user_agent}" "${prefix}/64/"  -o 064.html
+sleep 2; echo '065'; curl -s -A "${user_agent}" "${prefix}/65/"  -o 065.html
+sleep 2; echo '066'; curl -s -A "${user_agent}" "${prefix}/66/"  -o 066.html
+sleep 2; echo '067'; curl -s -A "${user_agent}" "${prefix}/67/"  -o 067.html
+sleep 2; echo '068'; curl -s -A "${user_agent}" "${prefix}/68/"  -o 068.html
+sleep 2; echo '069'; curl -s -A "${user_agent}" "${prefix}/69/"  -o 069.html
+sleep 2; echo '070'; curl -s -A "${user_agent}" "${prefix}/70/"  -o 070.html
+sleep 2; echo '071'; curl -s -A "${user_agent}" "${prefix}/71/"  -o 071.html
+sleep 2; echo '072'; curl -s -A "${user_agent}" "${prefix}/72/"  -o 072.html
+sleep 2; echo '073'; curl -s -A "${user_agent}" "${prefix}/73/"  -o 073.html
+sleep 2; echo '074'; curl -s -A "${user_agent}" "${prefix}/74/"  -o 074.html
+sleep 2; echo '075'; curl -s -A "${user_agent}" "${prefix}/75/"  -o 075.html
+sleep 2; echo '076'; curl -s -A "${user_agent}" "${prefix}/76/"  -o 076.html
+sleep 2; echo '077'; curl -s -A "${user_agent}" "${prefix}/77/"  -o 077.html
+sleep 2; echo '078'; curl -s -A "${user_agent}" "${prefix}/78/"  -o 078.html
+sleep 2; echo '079'; curl -s -A "${user_agent}" "${prefix}/79/"  -o 079.html
+sleep 2; echo '080'; curl -s -A "${user_agent}" "${prefix}/80/"  -o 080.html
+sleep 2; echo '081'; curl -s -A "${user_agent}" "${prefix}/81/"  -o 081.html
+sleep 2; echo '082'; curl -s -A "${user_agent}" "${prefix}/82/"  -o 082.html
+sleep 2; echo '083'; curl -s -A "${user_agent}" "${prefix}/83/"  -o 083.html
+sleep 2; echo '084'; curl -s -A "${user_agent}" "${prefix}/84/"  -o 084.html
+sleep 2; echo '085'; curl -s -A "${user_agent}" "${prefix}/85/"  -o 085.html
+sleep 2; echo '086'; curl -s -A "${user_agent}" "${prefix}/86/"  -o 086.html
+sleep 2; echo '087'; curl -s -A "${user_agent}" "${prefix}/87/"  -o 087.html
+sleep 2; echo '088'; curl -s -A "${user_agent}" "${prefix}/88/"  -o 088.html
+sleep 2; echo '089'; curl -s -A "${user_agent}" "${prefix}/89/"  -o 089.html
+sleep 2; echo '090'; curl -s -A "${user_agent}" "${prefix}/90/"  -o 090.html
+sleep 2; echo '091'; curl -s -A "${user_agent}" "${prefix}/91/"  -o 091.html
+sleep 2; echo '092'; curl -s -A "${user_agent}" "${prefix}/92/"  -o 092.html
+sleep 2; echo '093'; curl -s -A "${user_agent}" "${prefix}/93/"  -o 093.html
+sleep 2; echo '094'; curl -s -A "${user_agent}" "${prefix}/94/"  -o 094.html
+sleep 2; echo '095'; curl -s -A "${user_agent}" "${prefix}/95/"  -o 095.html
+sleep 2; echo '096'; curl -s -A "${user_agent}" "${prefix}/96/"  -o 096.html
+sleep 2; echo '097'; curl -s -A "${user_agent}" "${prefix}/97/"  -o 097.html
+sleep 2; echo '098'; curl -s -A "${user_agent}" "${prefix}/98/"  -o 098.html
+sleep 2; echo '099'; curl -s -A "${user_agent}" "${prefix}/99/"  -o 099.html
+sleep 2; echo '100'; curl -s -A "${user_agent}" "${prefix}/100/" -o 100.html
