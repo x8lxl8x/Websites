95d34a0b051e14c125d82da81e0cd63675e7ced2
x8lxl8x <58623820+x8lxl8x@users.noreply.github.com>
2022-05-09 07:07:04 +0300

    09.05.2022 07:07

--- home/programs/global-objects.php
+++ home/programs/global-objects.php
@@ -22,7 +22,7 @@ $dir_images             = "images";
 $file_index             = "index";
 $items_number           = 0;
 
-$user_agent             = 'Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0';
+$user_agent             = 'Mozilla/5.0 (X11; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0';
 
 #----------------------------------------------------------------------------------------------------
 
